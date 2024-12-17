ca42edee3167fcc79ae4dd0f617d14df59aa8fc6
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2024-12-17 12:07:46 +0100

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,6 +1,3 @@
 python -m pip install torch torchaudio --index-url https://download.pytorch.org/whl/cpu
-git clone https://github.com/descriptinc/audiotools
-python -m pip install audiotools
 python -m pip install -e .
-rm -rf audiotools
 python -m pip install --upgrade streamlit
